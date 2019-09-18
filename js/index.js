c95e13a6736952e931bc1f490cec83f237992bac
getmanovanton <getmanov11@mail.ru>
2019-09-18 10:59:56 +0300

    commit5

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
+
 var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
+
+var exitWindow = document.getElementById("exit");
+exitWindow.onclick = function () {
+    document.getElementById("sectionClose").style.display="none";
+}
